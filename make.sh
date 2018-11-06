6bd93548266f0a99af8ab7577372e83f962ef70a
Alexey Schebelev <iam@alexxnb.ru>
2018-11-07 00:40:46 +0300

    Ooops

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DEVSRV="root@teplogsm.ru"
-DEVSRVPATH="/var/www/sites/hisense.alexxnb.ru/radio10"
-
-DIR=$(dirname $0)
-if [[ $DIR == "." ]]; then DIR=$(pwd); fi;
-
-rsync -rlptzv --progress --delete --include=.htaccess --exclude=.* --exclude=*.sh  ${DIR}/ ${DEVSRV}:${DEVSRVPATH}
